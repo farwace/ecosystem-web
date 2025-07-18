2f2c1b30b12197777faef2c0108a10e8dee66716
panfilov.v <panfilov.v@bk.ru>
2025-07-18 13:56:30 +0300

    feat (404) - заглушка 404

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,19 +4,23 @@ import MainLayout from "@/layouts/MainLayout/MainLayout.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+      {
+        path: '/',
+        component: () => import('../layouts/MainLayout/MainLayout.vue'),
+        children: [{name: 'home', path: '', component: () => import('../pages/Home/HomePage.vue') }],
+      },
+      {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (About.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('../pages/AboutView.vue'),
+      },
     {
-      path: '/',
-      component: () => import('../layouts/MainLayout/MainLayout.vue'),
-      children: [{name: 'home', path: '', component: () => import('../pages/Home/HomePage.vue') }],
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../pages/AboutView.vue'),
-    },
+      path: '/:catchAll(.*)*',
+      component: () => import('../pages/ErrorNotFound.vue'),
+    }
   ],
 })
 
