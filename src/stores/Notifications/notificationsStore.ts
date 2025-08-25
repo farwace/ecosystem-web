266671aedd454e685b1b7f94e423e30690f722ae
panfilov.v <panfilov.v@bk.ru>
2025-08-25 17:31:38 +0300

    stub (svg) - Заглушки для генерации svg из страницы

--- src/stores/Notifications/notificationsStore.ts
+++ src/stores/Notifications/notificationsStore.ts
@@ -6,20 +6,17 @@ export const notificationsStore = defineStore('notifications', {
         notifications: {},
         isNotificationsInitialized: false,
         popups: {
-            'gift-page': {
+            'buy-money': {
                 canClose: true,
-                clientHeight: 567,
-                component: 'profile-gifts-popup',
+                clientHeight: 900,
+                component: 'shop-popup',
                 initialized: true,
                 isOpen: true,
                 data: {
                     darkBg: true,
-                    id: 1,
-                    avatar: 'https://sun70-1.userapi.com/s/v1/ig2/Mnbl4RlowjH_RK3F4WtScE7ZDNV1bEffN0zmkmJMPRlcSU1aSqCExpd3DgQ8pKFC1Lat8JMNx74crBuhsdZiCV7A.jpg?quality=95&crop=67,1,760,760&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720&ava=1&u=xcC0HAjSZgUX3g2E42IUjU3tG-JqrJWCoUrkvX060A8&cs=100x100',
-                    name: 'Виталий',
+                    modal: true,
                     noPaddings: true,
                     noTitle: true,
-                    pink: true,
                 }
             }
         },
