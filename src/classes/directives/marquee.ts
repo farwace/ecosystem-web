87a66e39a562293f2c8d9b5e7dbdf483053d447c
panfilov.v <panfilov.v@bk.ru>
2025-09-29 22:09:26 +0300

    fix (marquee.ts) - Добавил проверку, что bindings - строка

--- src/classes/directives/marquee.ts
+++ src/classes/directives/marquee.ts
@@ -1,16 +1,23 @@
 import type {DirectiveBinding, VNode} from "vue";
 import {Console} from "@/classes/utils/Console.ts";
+import {type} from "@colyseus/schema";
 
 export const vMarquee = {
     mounted: function (el:HTMLElement, binding: DirectiveBinding, vnode: VNode){
+        const doAction = (el: HTMLElement, className: string, vnode: VNode) => {
+            if(el.scrollWidth > el.clientWidth){
+                el.classList.add(className);
+                let mW = el.scrollWidth - el.clientWidth + 2;
+                el.style.setProperty('--marquee-width',  '-' + mW + 'px');
+                let speed = mW/5;
+                if(speed < 7) {speed = 7}
+                el.style.setProperty('--marquee-duration', speed + 's');
+            }
+        }
+
         Console.log(el, el.scrollWidth, el.clientWidth, binding.value);
-        if(el.scrollWidth > el.clientWidth){
-            el.classList.add(binding.value as string);
-            let mW = el.scrollWidth - el.clientWidth + 2;
-            el.style.setProperty('--marquee-width',  '-' + mW + 'px');
-            let speed = mW/5;
-            if(speed < 7) {speed = 7}
-            el.style.setProperty('--marquee-duration', speed + 's');
+        if(typeof binding.value === 'string'){
+            doAction(el, binding.value, vnode);
         }
     },
 }
